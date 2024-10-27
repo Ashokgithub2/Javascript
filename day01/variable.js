13e16914b4dc9abb2e728d566c34298ee00ab1f2
Ashokgithub2 <61586025+Ashokgithub2@users.noreply.github.com>
2024-10-27 18:55:29 +0530

    Add files via upload

---
+++ day01/variable.js
@@ -0,0 +1,21 @@
+const browser = "chrome"
+
+
+function setVersion(a){
+    
+     if ( a=="chrome"){
+    console.log("Inside Func :"+a)
+    const version = 23
+    console.log("Browser Version : "+version)
+     }
+}
+
+
+
+var a = 1 
+ 
+if (true){
+    var a = 7 
+    console.log(a)
+}
+console.log(a)
